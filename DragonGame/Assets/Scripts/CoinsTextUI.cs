b4313e604f75074438b4da1cf330573eb44b839c
Nikita Shpulin <shpulinn@mail.ru>
2024-08-10 20:40:29 +0200

    new scripts
    for showing coins, humans progress

---
+++ DragonGame/Assets/Scripts/CoinsTextUI.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class CoinsTextUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI coinsText;
+
+    private void OnEnable()
+    {
+        GlobalEventManager.OnCoinCollected.AddListener(UpdateText);
+    }
+
+    private void UpdateText(int amount)
+    {
+        if (coinsText)
+        {
+            coinsText.text = CoinsManager.Instance.CurrentCoins.ToString();
+        }
+    }
+}
---
+++ DragonGame/Assets/Scripts/HumansTextUI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class HumansTextUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI humansText;
+    
+    private void OnEnable()
+    {
+        GlobalEventManager.OnHumanCollected.AddListener(UpdateText);
+    }
+
+    private void Start()
+    {
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        if (humansText)
+        {
+            Invoke(nameof(DelayedTextUpdate), .1f);
+        }
+    }
+
+    private void DelayedTextUpdate()
+    {
+        humansText.text = $"{GameManager.Instance.CurrentHumanCatched.ToString()}/{GameManager.Instance.MaxHumanCatchAmount.ToString()}";
+    }
+}
