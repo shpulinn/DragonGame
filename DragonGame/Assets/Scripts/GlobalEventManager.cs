b3030e87febf396d8593c85e7c343e410c47394c
Nikita Shpulin <shpulinn@mail.ru>
2024-08-10 20:38:57 +0200

    global event manager script created

---
+++ DragonGame/Assets/Scripts/GlobalEventManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.Events;
+
+public class GlobalEventManager
+{
+    public static UnityEvent<int> OnCoinCollected = new UnityEvent<int>();
+    public static UnityEvent OnHumanCollected = new UnityEvent();
+
+    public static void SendCoinCollected(int amount = 0)
+    {
+        OnCoinCollected.Invoke(amount);
+    }
+
+    public static void SendHumanCollected()
+    {
+        OnHumanCollected.Invoke();
+    }
+}
