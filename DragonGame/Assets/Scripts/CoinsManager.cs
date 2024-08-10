ccf715e26f0805dd1ee9291ed37d158bc2cc2545
Nikita Shpulin <shpulinn@mail.ru>
2024-08-10 20:40:06 +0200

    coins manager script created

---
+++ DragonGame/Assets/Scripts/CoinsManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class CoinsManager : MonoBehaviour
+{
+    public static CoinsManager Instance;
+
+    public Event OnCoinAdd;
+    
+    private int _currentCoins;
+    
+    private SaveSystem _saveSystem;
+    private GameProgress _levelProgress;
+
+    public int CurrentCoins => _currentCoins;
+
+    private void OnEnable()
+    {
+        _saveSystem = new SaveSystem();
+        _levelProgress = _saveSystem.LoadGame();
+
+        _currentCoins = _levelProgress.Coins;
+        
+        GlobalEventManager.OnCoinCollected.AddListener(AddCoins);
+    }
+    
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.Log("More than 1 instance of CoinsManager");
+            return;
+        }
+        Instance = this;
+    }
+
+    public void AddCoins(int amount = 1)
+    {
+        if (amount < 1)
+        {
+            return;
+        }
+        _currentCoins += amount;
+        _levelProgress.Coins = _currentCoins;
+    }
+
+    public bool TrySpendCoins(int amount)
+    {
+        if (amount < 1)
+        {
+            Debug.Log("cant spend " + amount + " money");
+            return false;
+        }
+        if (_currentCoins - amount >= 0)
+        {
+            return false;
+        }
+
+        _currentCoins -= amount;
+        _levelProgress.Coins = _currentCoins;
+        return true;
+    }
+}
