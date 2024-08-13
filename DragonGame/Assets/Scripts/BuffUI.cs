c306d4be99cacf3dceb969a5ff4d79f7adfdd3b7
Nikita Shpulin <shpulinn@mail.ru>
2024-08-13 08:13:11 +0200

    BuffUI script

---
+++ DragonGame/Assets/Scripts/BuffUI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class BuffUI : MonoBehaviour
+{
+    [SerializeField] private GameObject buffUIHolder;
+    [SerializeField] private TextMeshProUGUI buffUIName;
+    [SerializeField] private UIManager _uiManager;
+
+    private void OnEnable()
+    {
+        GlobalEventManager.OnBuffEnabled.AddListener(ShowBuffUI);
+    }
+
+    // add icon?
+    private void ShowBuffUI(string buffName, float visibilityDuration)
+    {
+        buffUIName.text = buffName;
+        buffUIHolder.SetActive(true);
+        Invoke(nameof(HideBuffUI), visibilityDuration);
+        _uiManager.StartTimer(visibilityDuration);
+    }
+
+    private void HideBuffUI()
+    {
+        buffUIHolder.SetActive(false);
+        buffUIName.text = String.Empty;
+    }
+}
