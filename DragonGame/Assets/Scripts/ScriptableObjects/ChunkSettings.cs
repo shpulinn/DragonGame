0a7aa8a12498c24571c7eefb951a4df4a751fa96
Nikita Shpulin <shpulinn@mail.ru>
2024-08-13 08:11:32 +0200

    new scriptable objects sripts
    for dragon's buff and chunk settings

---
+++ DragonGame/Assets/Scripts/ScriptableObjects/ChunkSettings.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "ChunkSettings", menuName = "Chunks", order = 0)]
+    public class ChunkSettings : ScriptableObject
+    {
+        [Header("Шанс пустой точки спавна от 0 до 1")] [Range(0, 1)]
+        public float emptyChance = 0.1f;
+        [Header("Шанс спавна монет от 0 до 1")] [Range(0, 1)]
+        public float coinChance = .7f;
+        [Header("Шанс спавна людей от 0 до 1")] [Range(0, 1)]
+        public float humanChance = .5f;
+        [Header("Шанс спавна ловушек от 0 до 1")] [Range(0, 1)]
+        public float trapChance = .4f;
+    }
+}
---
+++ DragonGame/Assets/Scripts/ScriptableObjects/DragonBuffs.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "DragonBuff", menuName = "DragonBuffs", order = 0)]
+    public class DragonBuffs : ScriptableObject
+    {
+        [Header("Parameters")] 
+        public string buffName;
+        public Sprite buffIcon;
+        public float buffDuration;
+        [Header("Money")]
+        public bool enableCoinsMultiplier;
+        public int coinsMultiplier;
+        public bool enableDiamonds;
+        [Space] [Header("People")] 
+        public bool enableMoveThroughPeople;
+
+        [Space] [Header("Obstacles")] 
+        public bool enableMoveThroughObstacles;
+        public bool enableBreakingObstacles;
+
+        [Space] [Header("Other")] 
+        public bool enableSpawningSpecialChunk;
+    }
+}
