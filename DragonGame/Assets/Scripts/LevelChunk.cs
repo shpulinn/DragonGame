3545a6192a6f1d05c589e2f29053af36c84b34c6
Nikita Shpulin <shpulinn@mail.ru>
2024-08-13 08:14:50 +0200

    LevelChunk updated
    added logic for generating random objects (coins, humans, traps)

--- DragonGame/Assets/Scripts/LevelChunk.cs
+++ DragonGame/Assets/Scripts/LevelChunk.cs
@@ -1,17 +1,65 @@
+using System;
 using System.Collections.Generic;
+using ScriptableObjects;
 using UnityEngine;
 
 public class LevelChunk : MonoBehaviour
 {
+    [SerializeField] private ChunkSettings chunkSettings;
+    [SerializeField] private List<Transform> spawnPoints;
+    [SerializeField] private Transform diamondSpawnPoint;
+
+    [SerializeField] private GameObject coinPrefab;
+    [SerializeField] private GameObject humanPrefab;
+    [SerializeField] private List<GameObject> trapPrefabs;
+
     [SerializeField] private List<GameObject> coins;
     [SerializeField] private List<GameObject> humans;
+    [SerializeField] private List<GameObject> traps;
+
+    //TODO: добавить списки для объектов, которые гарантированно будут находиться на уровне
+    // например, группа монет или людей
 
     private void OnEnable()
     {
-        if (coins.Count > 0)
-            EnableEntities(coins);
-        if (humans.Count > 0)
-            EnableEntities(humans);
+        coins = new List<GameObject>();
+        humans = new List<GameObject>();
+        traps = new List<GameObject>();
+        for (int i = 0; i < spawnPoints.Count; i++)
+        {
+            float randomValue = UnityEngine.Random.value;
+            float totalChance = chunkSettings.coinChance + chunkSettings.humanChance + chunkSettings.trapChance + chunkSettings.emptyChance;
+
+            if (randomValue < chunkSettings.emptyChance / totalChance)
+            {
+                // nothing to spawn
+                continue;
+            }
+            else if (randomValue < (chunkSettings.emptyChance + chunkSettings.coinChance) / totalChance)
+            {
+                SpawnObject(coinPrefab, spawnPoints[i], coins);
+                continue;
+            }
+            else if (randomValue < (chunkSettings.emptyChance + chunkSettings.coinChance + chunkSettings.humanChance) / totalChance)
+            {
+                SpawnObject(humanPrefab, spawnPoints[i], humans);
+                continue;
+            }
+            else
+            {
+                GameObject randomTrapPrefab = trapPrefabs[UnityEngine.Random.Range(0, trapPrefabs.Count)];
+                SpawnObject(randomTrapPrefab, spawnPoints[i], traps);
+            }
+        }
+    }
+    
+    private void SpawnObject(GameObject prefab, Transform spawnPoint, List<GameObject> list = null)
+    {
+        GameObject spawnedObject = Instantiate(prefab, spawnPoint.position, Quaternion.identity, transform);
+        if (list != null)
+        {
+            list.Add(spawnedObject);
+        }
     }
 
     private void EnableEntities(List<GameObject> objects)
@@ -20,5 +68,25 @@ public class LevelChunk : MonoBehaviour
         {
             o.SetActive(true);
         }
+    }
+
+    private void OnDisable()
+    {
+        // clear chunk
+        foreach (var o in coins)
+        {
+            Destroy(o);
+        }
+        foreach (var o in humans)
+        {
+            Destroy(o);
+        }
+        foreach (var o in traps)
+        {
+            Destroy(o);
+        }
+        coins.Clear();
+        humans.Clear();
+        traps.Clear();
     }
 }
