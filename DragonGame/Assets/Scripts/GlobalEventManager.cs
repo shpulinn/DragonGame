d91ea0aba754d4e121d77ecccb24b2af8b0e680b
Nikita Shpulin <shpulinn@mail.ru>
2024-08-13 08:16:54 +0200

    2 new global events
    in GlobalEventManager

--- DragonGame/Assets/Scripts/GlobalEventManager.cs
+++ DragonGame/Assets/Scripts/GlobalEventManager.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine.Events;
+﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class GlobalEventManager
 {
     public static UnityEvent<int> OnCoinCollected = new UnityEvent<int>();
     public static UnityEvent OnHumanCollected = new UnityEvent();
+    public static UnityEvent<int> OnProgressReached = new UnityEvent<int>();
+    public static UnityEvent<string, float> OnBuffEnabled = new UnityEvent<string, float>();
 
     public static void SendCoinCollected(int amount = 0)
     {
@@ -13,5 +16,15 @@ public class GlobalEventManager
     public static void SendHumanCollected()
     {
         OnHumanCollected.Invoke();
+    }
+
+    public static void SendProgressReached(int amount = 0)
+    {
+        OnProgressReached.Invoke(amount);
+    }
+
+    public static void SendBuffEnabled(string name, float duration)
+    {
+        OnBuffEnabled.Invoke(name, duration);
     }
 }
